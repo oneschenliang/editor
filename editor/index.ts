f492defd881e7fb7ac2b273ee650436ac3ec7b8f
陈亮 <chenliang@ones.ai>
2022-09-02 17:43:51 +0800

    refactor:prepare webpack

--- editor/index.ts
+++ editor/index.ts
@@ -1,3 +1,5 @@
-export default function createEditor() {
-    
-}
+export default function createEditor() {}
+
+const root = document.createElement("div");
+root.innerText = "hello world";
+window.document.body.appendChild(root);
---
+++ webpack.editor.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./editor/index.ts",
+  target: "web",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "editor.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "editor/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        // include: path.join(__dirname, 'src/static/css'),
+        use: [
+          {
+            loader: "style-loader",
+            options: { attributes: { class: "editor-style-css" } },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: false,
+              namedExport: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    host: "127.0.0.1",
+    port: 7001,
+    open: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    onListening: function (devServer) {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      const port = devServer.server.address().port;
+      console.log('Listening on port:', port);
+    },
+  },
+};
